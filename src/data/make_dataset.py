e578087e174f08faea382cf35c92b1be4cead9a1
RyanAugust <rad19206@yahoo.com>
2023-05-31 10:14:35 -0700

    completed make_dataset with `CheetahPyAnalytics` as heavy lifter

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
-import click
+# import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+# from dotenv import find_dotenv, load_dotenv
+
+import CheetahPyAnalytics
+import pandas as pd
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+# @click.command()
+# @click.argument('input_filepath', type=click.Path(exists=True))
+# @click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
@@ -15,6 +18,20 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+    static_metrics = {"max_hr": 191
+                 ,"resting_hr": 39
+                 ,'ae_threshold_hr': 145
+                 ,'LTthreshold_hr': 160
+                 ,'ae_threshold_pwr': 252
+                 ,'LTthreshold_pwr': 302
+                 ,'run_settings':{'cp': 356
+                                 ,'w_prime': 16900
+                                 ,'pmax': 642}}
+
+    datapp = CheetahPyAnalytics.dataset_preprocess(local_activity_store=input_filepath)
+    datapp.pre_process(load_metric="Daily_TSS", performance_metric="VO2", fill_performance_forward=False)
+
+    datapp.processed_activity_data.to_csv(output_filepath)
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -25,6 +42,7 @@ if __name__ == '__main__':
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    # load_dotenv(find_dotenv())
 
-    main()
+    main(input_filepath=Path.joinpath(project_dir, 'data/raw/activity_data.csv')
+         ,output_filepath=Path.joinpath(project_dir, 'data/processed/processed_activity_data.csv'))
