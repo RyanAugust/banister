32804a540ae9d1312281107427394ece40aaee8e
RyanAugust <rad19206@yahoo.com>
2023-05-31 14:48:18 -0700

    module imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,18 @@
-from ..models.banister_model import banister_model
+__title__ = 'banister'
+__version__ = '0.0.1'
+__author__ = 'RyanAugust'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2023'
+
+from .models import (train_model,
+                    predict_model)
+
+from .data import (make_dataset,
+                  )
+
+
+__all__ = [
+    'train_model',
+    'predict_model',
+    'make_dataset'
+]
